f39ce3f5836906cf8bb8c304a25a5205c8931692
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-11-01 15:41:54 +0100

    Added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Layout from './components/Layout.jsx';
+import ScreenLoader from './components/ScreenLoader.jsx';
+
+const MainPage = lazy(() => import('./pages/MainPage.jsx'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage.jsx'));
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage.jsx'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage.jsx'));
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Suspense fallback={<ScreenLoader />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default App;
---
+++ src/components/ScreenLoader.jsx
@@ -0,0 +1,5 @@
+const ScreenLoader = () => {
+  return <div>Loading...</div>;
+};
+
+export default ScreenLoader;
